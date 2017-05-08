d1e3143ff30b10ba85ceeb478f2e66b2a8afba42
Yijiang Huang <yijiangh@mit.edu>
2017-05-08 15:37:28 -0400

    init repo, refactor from navfn.

---
+++ include/move_incremental/potarr_point.h
@@ -0,0 +1,60 @@
+//
+// Created by student on 5/8/17.
+//
+/*********************************************************************
+*
+* Software License Agreement (BSD License)
+*
+*  Copyright (c) 2008, (Simon) Ye Cheng
+*  All rights reserved.
+*
+*  Redistribution and use in source and binary forms, with or without
+*  modification, are permitted provided that the following conditions
+*  are met:
+*
+*   * Redistributions of source code must retain the above copyright
+*     notice, this list of conditions and the following disclaimer.
+*   * Redistributions in binary form must reproduce the above
+*     copyright notice, this list of conditions and the following
+*     disclaimer in the documentation and/or other materials provided
+*     with the distribution.
+*   * Neither the name of the Willow Garage nor the names of its
+*     contributors may be used to endorse or promote products derived
+*     from this software without specific prior written permission.
+*
+*  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+*  "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+*  LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+*  FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+*  COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+*  INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+*  BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+*  LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+*  CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+*  LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+*  ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+*  POSSIBILITY OF SUCH DAMAGE.
+*
+*********************************************************************/
+#ifndef MOVEINCREMENTAL_POTARR_POINT_H
+#define MOVEINCREMENTAL_POTARR_POINT_H
+
+#include <pcl/register_point_struct.h>
+
+namespace move_incremental {
+    struct PotarrPoint {
+        float x;
+        float y;
+        float z;
+        float pot_value;
+    };
+}
+
+POINT_CLOUD_REGISTER_POINT_STRUCT(
+        move_incremental::PotarrPoint,
+        (float, x, x)
+        (float, y, y)
+        (float, z, z)
+        (float, pot_value, pot_value));
+
+#endif //PROJECT_POTARR_POINT_H
